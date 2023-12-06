a013dda93c4a8834c53f4d16956a1e60006df5c9
fabian-dios <fabian@iofinnet.com>
2023-12-06 18:50:27 +0100

    fix: removed commented code on day 6

--- tests/day-6/desertIsland.test.ts
+++ tests/day-6/desertIsland.test.ts
@@ -4,22 +4,10 @@ import {
 } from "../../src/day-6/desertIsland";
 
 describe("Test desertIsland", () => {
-  // it("example part assertion", async () => {
-  //     const summatory = await calculateMinimumSeedLocationForAlmanac(
-  //         "./tests/day-5/example",
-  //     );
-  //     expect(summatory).toEqual(35);
-  // });
   it("first part assertion", async () => {
     const summatory = await calculateMarginOfError("./tests/day-6/input");
     expect(summatory).toEqual(2374848);
   });
-  // it("second part example assertion", async () => {
-  //     const summatory = await calculateMinimumSeedLocationForAlmanacPart2(
-  //         "./tests/day-5/example",
-  //     );
-  //     expect(summatory).toEqual(46);
-  // });
   it("second part assertion", async () => {
     const summatory = await calculateMarginOfErrorPart2("./tests/day-6/input");
     expect(summatory).toEqual(59370572);
